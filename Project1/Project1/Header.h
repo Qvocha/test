54a4bea91e493254e0a77e0d4fed705af3c0ad94
Qvocha <pineappletoe@gmail.com>
2019-10-01 13:06:48 +0300

    delete all

--- Project1/Project1/Header.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace mySpace
-{
-	int func(int x);
-}
--- Project1/Sample-Test1/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "../Project1/Header.h"
-
-TEST(TestCaseName, TestName)
-{
-  EXPECT_EQ(15, 15);
-}
